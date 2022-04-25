252096f2781bc4db001526ea5f03b47019908549
ShiningRay <tsowly@hotmail.com>
2022-04-25 19:26:21 +0800

    update task

--- config/initializers/balance_updater.rb
+++ config/initializers/balance_updater.rb
@@ -4,3 +4,8 @@ BalanceUpdater = Concurrent::TimerTask.new(execution_interval: rand(2.hours..4.h
   UpdateOfficialAccountBalanceService.call
 end
 BalanceUpdater.execute
+
+Sender =  Concurrent::TimerTask.new(execution_interval: 10) do
+  SendCapacityService.new.call
+end
+Sender.execute
