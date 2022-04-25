bfdfd65346452943ee609ad59f8f4bbc2f8a6480
ShiningRay <tsowly@hotmail.com>
2022-04-25 19:36:43 +0800

    use separate process

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec puma -C config/puma.rb
+scheduler: bundle exec ruby lib/claim_event_processor.rb
--- lib/claim_event_processor.rb
+++ lib/claim_event_processor.rb
@@ -2,7 +2,16 @@
 
 require_relative "../config/environment"
 
+BalanceUpdater = Concurrent::TimerTask.new(execution_interval: rand(2.hours..4.hours)) do
+  puts 'Update balance'
+  ActiveRecord::Base.connection_pool.with_connection do
+    UpdateOfficialAccountBalanceService.call
+  end
+end
+BalanceUpdater.execute
+
 loop do
+  puts 'Sending capacity'
   SendCapacityService.new.call
   sleep(10)
 end
