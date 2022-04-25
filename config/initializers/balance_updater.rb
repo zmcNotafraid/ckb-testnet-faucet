91784e29f8f0b3a1da9f03fe5d1dcd4a17986ca5
ShiningRay <tsowly@hotmail.com>
2022-04-25 19:32:08 +0800

    db pool

--- config/initializers/balance_updater.rb
+++ config/initializers/balance_updater.rb
@@ -1,11 +1,17 @@
 
 
 BalanceUpdater = Concurrent::TimerTask.new(execution_interval: rand(2.hours..4.hours)) do
-  UpdateOfficialAccountBalanceService.call
+  puts 'Update balance'
+  ActiveRecord::Base.connection_pool.with_connection do
+    UpdateOfficialAccountBalanceService.call
+  end
 end
 BalanceUpdater.execute
 
 Sender =  Concurrent::TimerTask.new(execution_interval: 10) do
-  SendCapacityService.new.call
+  puts 'Sending capacity'
+  ActiveRecord::Base.connection_pool.with_connection do
+    SendCapacityService.new.call
+  end
 end
 Sender.execute
