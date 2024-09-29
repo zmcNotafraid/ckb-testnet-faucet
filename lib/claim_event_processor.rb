cf06222c9303078f27eff1ee0a6cf9cf50cf6472
Miles Zhang <mingchang555@hotmail.com>
2024-09-29 17:38:06 +0800

    feat: commit offical account balance update
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/claim_event_processor.rb
+++ lib/claim_event_processor.rb
@@ -2,13 +2,13 @@
 
 require_relative "../config/environment"
 
-BalanceUpdater = Concurrent::TimerTask.new(execution_interval: 3.hour) do
-  puts 'Sync Offical Account Balance'
-  ActiveRecord::Base.connection_pool.with_connection do
-    UpdateOfficialAccountBalanceService.call
-  end
-end
-BalanceUpdater.execute
+# BalanceUpdater = Concurrent::TimerTask.new(execution_interval: 3.hour) do
+#   puts 'Sync Offical Account Balance'
+#   ActiveRecord::Base.connection_pool.with_connection do
+#     UpdateOfficialAccountBalanceService.call
+#   end
+# end
+# BalanceUpdater.execute
 
 loop do
   puts "Check Offical Account Balance Enough"
